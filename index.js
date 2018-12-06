5ef7fc704666c1abf4d21c7272c2fe1dd014bc67
violasafa <45582612+violasafa@users.noreply.github.com>
2018-12-06 11:01:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-//const port = 3000
+const port = 3000
 
 app.use(bodyParser.json())
 app.use(
@@ -21,4 +21,4 @@ app.post('/users', db.createUser)
 app.put('/users/:id', db.updateUser)
 app.delete('/users/:id', db.deleteUser)
 
-//app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000)
